f10f89118b855e64684ef9b8b2721858669f145b
zul <zul0925@qq.com>
2022-12-07 09:20:50 +0800

    nth

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,34 @@
+<!--
+ * @Author: zul zul0925@qq.com
+ * @Date: 2022-12-05 14:32:56
+ * @LastEditors: zul zul0925@qq.com
+ * @LastEditTime: 2022-12-05 14:44:35
+ * @FilePath: \wt\src\App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <div class="bg"></div>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+*{
+    margin: 0;
+    padding: 0;
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+#app{
+    background-image: url("./assets/texture.png");
+    background-color: rgb(246, 206, 193);
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    .bg{
+        background-image: url("./assets/peach-bg.png");
+        background-size: contain;
+        width: 100%;
+        opacity: .2;
+        flex: auto;
     }
-  }
 }
 </style>
